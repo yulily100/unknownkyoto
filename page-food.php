d33a6a753bd9e5902f4ea571312c75bcc852548c
yulily100 <yulililily100@yahoo.co.jp>
2020-01-20 18:44:00 +0900

    php

---
+++ page-food.php
@@ -0,0 +1,151 @@
+<?php get_header(); ?>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+
+<script>
+$(function() {
+
+  // instagram feed
+  var accessToken = '10677467838.a39ffa6.79cee58175a245f994d8e86f61a27698'; //token
+  $.getJSON('https://api.instagram.com/v1/users/self/media/recent/?access_token='+accessToken+'&callback=?',function (insta) {
+    $.each(insta.data,function (photos,src) {
+      if ( photos === 4 ) { return false; }
+      $('<li><a href="'+src.link+'" target="_blank"><img src="'+src.images.standard_resolution.url+'"></a></li>').appendTo('#js-instafeed');
+    });
+  });
+
+  // ページ内をなめらかにスクロール
+  $('a[href^="#"]').click(function() {
+     var speed = 600;
+     var href= $(this).attr("href");
+     var target = $(href == "#" || href == "" ? 'html' : href);
+     var position = target.offset().top;
+     $('body,html').animate({scrollTop:position}, speed, 'swing');
+     return false;
+  });
+
+});
+</script>
+
+
+<div class="container">
+  <div class="page-header-food page-food">
+    <div class="page-header-inner">
+      <h1 class="page-food-title"><a href="/coworking"><span>UNKNOWN KYOTO</span>Lunch & Dining Bar</a></h1>
+      <p class="page-food-hours">11:30 - 15:00 (L.O. 14:30)<br>
+      17:00 - 23:00 (L.O. 22:00)</p>
+    </div>
+  </div>
+</div>
+
+<div class="wrapper">
+  <div class="coworking-about">
+    <p class="coworking-about-description">
+      UNKNOWN KYOTOの飲食スペースには<br>
+      系統の違う２つのお店が入ります。<br>
+      安心してください、注文も会計も同じ場所です。</p>
+    <div class="coworking-about-image">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/food/food-main.jpg" alt="飲食店インテリア" class="for-pc">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/food/food-main-sp.jpg" alt="飲食店インテリア" class="for-sp">
+    </div>
+  </div>
+  <div class="food-contents">
+    <section class="food-contents-lunch">
+      <h2 class="food-contents-title-lunch">Lunch Time</h2>
+      <p class="food-contents-hours">11:30 - 15:00 (L.O. 14:30)</p>
+      <ul class="food-type">
+        <li>
+          <p class="food-type-label">定食メニュー</p>
+          <img src="<?php echo get_template_directory_uri(); ?>/img/food/lunch-1.jpg" alt="唐揚げランチ">
+          <div class="food-type-text-sin">
+            <h3 class="food-type-title">Sin -Dining＆Bar-</h3>
+            <ul class="food-type-list">
+              <li><span>唐揚げランチ</span><span class="food-type-price">800円</span></li>
+              <li>他にも数種類ランチ定食あります。</li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <p class="food-type-label">スパイスカレー</p>
+          <img src="<?php echo get_template_directory_uri(); ?>/img/food/lunch-2.jpg" alt="スパイスカレー">
+          <div class="food-type-text-spiceordie">
+            <h3 class="food-type-title">スパイスオアダイ</h3>
+            <ul class="food-type-list">
+              <li><span>日替わりスパイスプレート</span><span class="food-type-price">1000円</span></li>
+              <li>一種・あいがけが選べます。</li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </section>
+    <section class="food-contents-lunch">
+      <h2 class="food-contents-title-dinner">Dinner Time</h2>
+      <p class="food-contents-hours">17:00 - 23:00 (L.O. 22:00)</p>
+      <ul class="food-type">
+        <li>
+          <p class="food-type-label">本格イタリアン</p>
+          <img src="<?php echo get_template_directory_uri(); ?>/img/food/dinner-1.jpg" alt="唐揚げランチ">
+          <div class="food-type-text-sin">
+            <h3 class="food-type-title">Sin -Dining＆Bar-</h3>
+            <ul class="food-type-list">
+              <li><span>ベビーホタテと九条ネギのアヒージョ</span><span class="food-type-price">700円</li>
+              <li><span>ポッロフリット（唐揚げ）</span><span class="food-type-price">600円</li>
+              <li><span>カルボナーラ</span><span class="food-type-price">1100円</li>
+              <li><span>京都ポーク肩ロース肉のグリル</span><span class="food-type-price">1500円</li>
+              <li>他にもメニュー多数あります。</li>
+            </ul>
+          </div>
+        </li>
+        <li>
+          <p class="food-type-label">イザカヤ</p>
+          <img src="<?php echo get_template_directory_uri(); ?>/img/food/dinner-2.jpg" alt="スパイスカレー">
+          <div class="food-type-text-unknownshokudo">
+            <h3 class="food-type-title">スパイスオアダイ</h3>
+            <ul class="food-type-list">
+              <li><span>ぽてさら</span><span class="food-type-price">350円</li>
+              <li><span>お造り盛り合せ</span><span class="food-type-price">650円</li>
+              <li><span>とろとろレバー</span><span class="food-type-price">450円</li>
+              <li><span>なっとう出汁巻き</span><span class="food-type-price">550円</li>
+              <li>他にもメニュー多数あります。</li>
+            </ul>
+          </div>
+        </li>
+      </ul>
+    </section>
+  </div>
+</div>
+
+<section class="food-detail">
+  <div class="food-detail-img">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/food/interior-1.jpg" alt="立ち飲み席">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/food/interior-2.jpg" alt="テーブル席">
+  </div>
+  <div class="food-detail-text">
+    <h3 class="food-detail-title">営業時間</h3>
+    <p class="food-detail-description">11:30 - 15:00 (L.O. 14:30)<br>
+      17:00 - 23:00 (L.O. 22:00)</p>
+    <p class="food-detail-description">日曜定休</p>
+    <h3 class="food-detail-title">店舗規模</h3>
+    <p class="food-detail-description">テーブル席　◯席<br>
+      立ち飲み席　◯席<br>
+      カウンター席　◯席</p>
+    <p class="food-detail-description">立ち飲み席には椅子を出すこともできます。</p>
+    <h3 class="food-detail-title">ご連絡</h3>
+    <p class="food-detail-description">075-746-4665</p>
+    <p class="food-detail-description">お問い合わせ・ご予約はこちらまで。</p>
+    <div class="food-detail-access">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.4799155063033!2d135.76624239999998!3d34.994689799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x79557a17e1c2815d!2sUNKNOWN%20KYOTO!5e0!3m2!1sja!2sjp!4v1578797119229!5m2!1sja!2sjp" width="auto" height="200" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      <p class="food-detail-access-btn"><a href="https://goo.gl/maps/9aVuTd8iJGMXpK9f7">google mapで表示する</a></p>
+    </div>
+  </div>
+</section>
+<div class="wrapper">
+  <section class="food-instafeed">
+    <ul id="js-instafeed" class="food-instafeed-list">
+    </ul>
+    <p class="section-link"><a href="https://www.instagram.com/unknownkyoto/" target="_blank" rel="noopener">Follow on Instagram @unknownkyoto</a></p>
+  </section>
+</div>
+
+
+<?php get_footer(); ?>
