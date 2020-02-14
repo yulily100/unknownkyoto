8da46f5ab1279fd7bec7635af71094f91a3814a3
yulily100 <yulililily100@yahoo.co.jp>
2020-02-14 13:56:06 +0900

    タイトル属性調整

--- page.php
+++ page.php
@@ -3,7 +3,7 @@
 	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 		<div class="wrapper">
 			<article class="page-basic">
-				<h2 class="page-basic-title"><?php echo get_the_title(); ?></h2>
+				<h1 class="page-basic-title"><?php echo get_the_title(); ?></h1>
 				<div class="page-basic-contents">
 					<?php the_content(); ?>
 				</div>
