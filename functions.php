6fadf7e6117222ad6af3998d57d379bd23e58212
yulily100 <yulililily100@yahoo.co.jp>
2020-07-07 10:19:01 +0900

    日本語URL禁止

--- functions.php
+++ functions.php
@@ -35,4 +35,13 @@
 		unset($menu[25]); // コメント
 	}
 	add_action('admin_menu', 'remove_menus');
+
+	// 日本語URL禁止
+	function auto_post_slug( $slug, $post_ID, $post_status, $post_type ) {
+    if ( preg_match( '/(%[0-9a-f]{2})+/', $slug ) ) {
+        $slug = utf8_uri_encode( $post_type ) . '-' . $post_ID;
+    }
+    return $slug;
+	}
+	add_filter( 'wp_unique_post_slug', 'auto_post_slug', 10, 4  );
 ?>
