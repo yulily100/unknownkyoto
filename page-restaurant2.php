f41808c25c6840748d3f0a8abf1cb93a87c227e2
yulily100 <yulily100@users.noreply.github.com>
2020-10-06 14:34:44 +0900

    レストラン

---
+++ page-restaurant2.php
@@ -0,0 +1,180 @@
+<?php get_header(); ?>
+
+<div class="container">
+  <div class="restaurant-header">
+    <h1><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant-logo.svg" alt="Sin -Dinging & Bar-" width="150" height="150"></h1>
+  </div>
+
+  <div class="restaurant-wrapper">
+    <div class="restaurant-intro">
+      <div class="restaurant-intro-img">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/intro.jpg" alt="ディナーではパスタ、ピザ、お肉料理など、本格イタリアンが楽しめます。">
+      </div>
+      <div class="restaurant-intro-text">
+        <h2 class="restaurant-intro-title">昼は定食でお腹いっぱいに<br>
+          夜は本格的なイタリアンで乾杯</h2>
+        <p class="restaurant-intro-description">京都清水五条のレストラン<strong>「Sin -Dining＆Bar-」</strong>です。<br>UNKNOWN KYOTOの1階にあります。</p>
+        <p class="restaurant-intro-description">元遊郭建築をリノベーションした店内で、昼間はお腹いっぱい食べられる定食や丼ものを、夜は本格的なイタリアンとお酒をお楽しみください。</p>
+        <p class="restaurant-intro-description">レストランは宿泊者以外でも、誰でもお楽しみいただけます。テイクアウトやパーティも承っております。お気軽にご相談ください。</p>
+        <div class="restaurant-intro-wrapper">
+          <div class="restaurant-intro-open">
+            <h3 class="restaurant-intro-open-title">Lunch</h3>
+            <p class="restaurant-intro-open-time">11:30 - 15:00<br>
+              L.O. 14:30</p>
+          </div>
+          <div class="restaurant-intro-open">
+            <h3 class="restaurant-intro-open-title">Dinner</h3>
+            <p class="restaurant-intro-open-time">17:00 - 23:00</br>
+              L.O. お食事22:00,ドリンク22:30</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="restaurant-wrapper">
+    <div class="restaurant-photo">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/photo1.jpg" alt="">
+      <div class="restaurant-photo-h">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/photo-h-1.jpg" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/photo-h-2.jpg" alt="">
+      </div>
+      <img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/photo2.jpg" alt="">
+      <div class="restaurant-photo-h">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/photo-h-3.jpg" alt="">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/photo-h-4.jpg" alt="">
+      </div>
+      <img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/photo3.jpg" alt="">
+    </div>
+  </div>
+
+
+  <div class="restaurant-contents">
+    <div class="restaurant-contents-img">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/lunch.jpg" alt="ディナーではパスタ、ピザ、お肉料理など、本格イタリアンが楽しめます。">
+    </div>
+
+    <div class="restaurant-contents-text">
+      <div class="restaurant-contents-right">
+        <h2 class="restaurant-contents-title-lunch">Lunch Time</h2>
+        <p class="restaurant-contents-open">11:30 - 15:00</p>
+        <p class="restaurant-contents-description">お腹いっぱい味わえる定食や丼ものをご用意しております。お仕事の合間やおひとりでのランチにも気軽にご利用ください。一番人気は唐揚げ定食。唐揚げ専門店で7年間腕を磨いたシェフの逸品をお楽しみください。</p>
+        <div class="restaurant-contents-menu">
+          <h3 class="restaurant-contents-menu-title">Menu</h3>
+          <p class="restaurant-contents-menu-item">唐揚げ定食 / ハンバーグ定食 / アジフライ定食<br>
+            生姜焼き定食 / 牛ステーキ丼 / ロコモコ風丼　など</p>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="restaurant-contents">
+    <div class="restaurant-contents-text">
+      <div class="restaurant-contents-left">
+        <h2 class="restaurant-contents-title-dinner">Dinner Time</h2>
+        <p class="restaurant-contents-open">17:00 - 23:00</p>
+        <p class="restaurant-contents-description">
+          夜は本格的なイタリアンとお酒で特別なひとときを。ちょっとしたアテにもなる和風居酒屋メニューもございます。<br>
+          ご友人やご家族同士でわいわい楽しむだけでなく、普通の夕食としてパスタのみ注文、といったスタイルも歓迎しています。
+        </p>
+      </div>
+    </div>
+    <div class="restaurant-contents-img">
+      <img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/dinner.jpg" alt="ディナーではパスタ、ピザ、お肉料理など、本格イタリアンが楽しめます。">
+    </div>
+  </div>
+  <div class="restaurant-menuphoto-wrapper">
+    <ul class="restaurant-menuphoto">
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/05.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/09.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/04.jpg" alt="サラダ"></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/13.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/06.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/10.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/08.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/01.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/11.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/14.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/02.jpg" alt=""></li>
+    </ul>
+    <ul class="restaurant-menuphoto">
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/05.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/09.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/04.jpg" alt="サラダ"></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/13.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/06.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/10.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/08.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/01.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/11.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/14.jpg" alt=""></li>
+      <li><img src="<?php echo get_template_directory_uri(); ?>/img/restaurant/menu/02.jpg" alt=""></li>
+    </ul>
+  </div>
+  <div class="restaurant-wrapper">
+    <div class="restaurant-menu">
+      <div class="restaurant-menu-contents">
+        <h3 class="restaurant-menu-title">Italian</h3>
+        <ul class="restaurant-menu-list">
+          <li>アンチョビポテトサラダ<span class="restaurant-menu-price">400円</span></li>
+          <li>ポッロフリット（唐揚げ）<span class="restaurant-menu-price">600円</span></li>
+          <li>パスタ<span class="restaurant-menu-price">900円〜</span></li>
+          <li>ピザ<span class="restaurant-menu-price">900円〜</span></li>
+          <li>メインのお肉料理<span class="restaurant-menu-price">1200円〜</span></li>
+          <li>その他</li>
+        </ul>
+      </div>
+      <div class="restaurant-menu-contents">
+        <h3 class="restaurant-menu-title">Izakaya menu</h3>
+        <ul class="restaurant-menu-list">
+          <li>枝豆<span class="restaurant-menu-price">350円</span></li>
+          <li>酒盗クリームチーズ<span class="restaurant-menu-price">400円</span></li>
+          <li>茄子と玉ねぎの甘酢漬け<span class="restaurant-menu-price">450円</span></li>
+          <li>やわらか鶏レバー<span class="restaurant-menu-price">550円</span></li>
+          <li>とり天からしポン酢<span class="restaurant-menu-price">600円</span></li>
+          <li>その他</li>
+        </ul>
+      </div>
+      <div class="restaurant-menu-contents">
+        <h3 class="restaurant-menu-title">Drink</h3>
+        <ul class="restaurant-menu-list">
+          <li>生ビール<span class="restaurant-menu-price">550円</span></li>
+          <li>自家製サングリア<span class="restaurant-menu-price">650円</span></li>
+          <li>サワー・酎ハイ各種<span class="restaurant-menu-price">550円</span></li>
+          <li>焼酎<span class="restaurant-menu-price">500円</span></li>
+          <li>ソフトドリンク各種<span class="restaurant-menu-price">350円</span></li>
+          <li>その他</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <section class="food-detail">
+  <div class="food-detail-img">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/food/interior-1.jpg" alt="立ち飲み席">
+    <img src="<?php echo get_template_directory_uri(); ?>/img/food/interior-2.jpg" alt="テーブル席">
+  </div>
+  <div class="food-detail-text">
+    <h3 class="food-detail-title">営業時間</h3>
+    <p class="food-detail-description">
+      11:30 - 15:00 (L.O. 14:30)<br>
+      17:00 - 23:00 (L.O. お食事22:00,ドリンク22:30)</p>
+    <p class="food-detail-description">日曜定休</p>
+    <h3 class="food-detail-title">店舗規模</h3>
+    <p class="food-detail-description">テーブル席　8席<br>
+      立ち飲み席　12席<br>
+      カウンター席　8席<br>
+      おひとりでも利用しやすいカウンター席、最大8人ほどでご利用いただけるテーブル席、大人数でも対応可能な立ち飲み席がございます。</p>
+    <p class="food-detail-description">立ち飲み席には椅子を出すこともできます。</p>
+    <h3 class="food-detail-title">ご連絡</h3>
+    <p class="food-detail-description">075-746-4665</p>
+    <p class="food-detail-description">お問い合わせ・ご予約はこちらまで。</p>
+    <div class="food-detail-access">
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3268.4799155063033!2d135.76624239999998!3d34.994689799999996!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x79557a17e1c2815d!2sUNKNOWN%20KYOTO!5e0!3m2!1sja!2sjp!4v1578797119229!5m2!1sja!2sjp" width="auto" height="250" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      <p class="food-detail-access-btn"><a href="https://goo.gl/maps/9aVuTd8iJGMXpK9f7">google mapで表示する</a></p>
+    </div>
+  </div>
+</section>
+
+  
+
+<?php get_footer(); ?>
