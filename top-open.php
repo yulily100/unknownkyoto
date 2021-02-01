cfbe2117eaed9a8dd6efd5566457f81e90fe4b4a
yulily100 <yulily100@users.noreply.github.com>
2021-02-01 09:20:12 +0900

    オープン時間

--- top-open.php
+++ top-open.php
@@ -3,7 +3,7 @@
     <ul class="top-open-contents">
       <li class="top-open-heading">
         <h2 class="top-open-title">営業時間</h2>
-        <p class="top-open-note">京都府の要請によりレストランの営業時間を短縮しています。</p>
+        <p class="top-open-note">緊急事態宣言によりレストランの営業時間を短縮しています。</p>
         <p class="top-open-note">37度以上の熱がある方のご来店をお断りしております。</p>
       </li>
       <li class="top-open-body">
@@ -17,7 +17,7 @@
         <h3 class="top-open-contents-title">
           <a href="/restaurant">Restaurant</a>
         </h3>
-        <p class="top-open-contents-description">11:30 - 15:00 (L.O. 14:30)<br>17:00 - <span class="limited">21:00</span><br>
+        <p class="top-open-contents-description">11:30 - 15:00 (L.O. 14:30)<br>17:00 - <span class="limited">20:00</span><br>
       <span class="top-open-contents-note">日曜定休</span>
         </p>
       </li>
