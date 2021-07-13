01212e1cd3944faed330fda7f80d2c8ed4d87083
yulily100 <yulily100@users.noreply.github.com>
2021-07-13 10:29:15 +0900

    ワーケーションページ追加

---
+++ page-workation.php
@@ -0,0 +1,171 @@
+<?php get_header(); ?>
+<div class="container">
+  <header class="workation-header">
+    <div class="workation-header-text">
+      <h1 class="workation-header-title">Work + Vacation</h1>
+      <p class="workation-header-desc">しっかりと仕事できる宿</p>
+    </div>
+  </header>
+
+  <div class="workation-wrapper">
+    <p class="workation-intro-desc">京都・清水五条駅すぐの「UNKNOWN KYOTO」は<br>
+      京都でのワーケーションや合宿に最適の宿です。<br>
+      コワーキングスペースがあり、<br>
+      宿泊者は24時間自由に利用可能です。</p>
+    <p class="workation-intro-img"><img src="<?php echo get_template_directory_uri(); ?>/img/workation/intro-top.jpg" alt="コワーキング"></p>
+    <p class="workation-intro-desc">日中はほどよく人の気配がありながらも、<br>
+      カフェやオープンなロビーとは違った<br>
+      しっかりと仕事に集中できる空間。<br>
+      この雰囲気を実現するため<br>
+      あえてBGMを流さないようにしています。</p>
+    <p class="workation-intro-desc">一方で仕事する人の少ない朝や夜はくつろぎの時間に。<br>
+      宿泊者と施設利用者の間で交流があったりと、<br>
+      観光だけの施設にはない出会いがあるかもしれません。</p>
+    <div class="workation-intro-img-bottom">
+      <div class="workation-intro-img-left">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/workation/intro-left-1.jpg" alt="" class="top">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/workation/intro-left-2.jpg" alt="">
+      </div>
+      <div class="workation-intro-img-right">
+        <img src="<?php echo get_template_directory_uri(); ?>/img/workation/intro-right.jpg" alt="">
+      </div>
+    </div>
+    <p class="workation-intro-desc">もちろんwifiと電源完備。会議室も利用可能。<br>
+      重厚なワークチェアはありませんが、<br>
+      様々な種類の椅子を揃えています。<br>
+      体に合うものを探してみてください。</p>
+    <ul class="workation-facilities">
+      <li>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/workation/facilities-1.jpg" alt="お部屋">
+        <p>元お茶屋さん建築をリノベーションした京都らしいお部屋。個室とドミトリーがあります。</p>
+        <p><a href="/hostel">お部屋の詳細はこちら</a></p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/workation/facilities-2.jpg" alt="レストラン">
+        <p>施設内にしっかりと食べられるレストランがあります。名物の唐揚げをぜひ一度食べてみてください。</p>
+        <p><a href="/restaurant">レストランの詳細はこちら</a></p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/workation/facilities-3.jpg" alt="キッチン">
+        <p>キッチンと調理器具一式があります。簡単な自炊が可能です。</p>
+        <p>電子レンジ / 冷蔵庫 / 電気湯沸かし器 / 浄水器 / コンロ / 炊飯器</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/workation/facilities-4.jpg" alt="">
+        <p>会議室が二箇所ございます。通常は1時間1,100円ですが、合宿プランを利用の場合は1日2時間まで無料です。</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/workation/facilities-5.jpg" alt="">
+        <p>館内に館内にコインランドリーがあります</p>
+        <p>洗濯機1回200円、ガス乾燥機30分100円</p>
+      </li>
+      <li>
+        <img src="<?php echo get_template_directory_uri(); ?>/img/workation/facilities-6.jpg" alt="">
+        <p>徒歩3分の場所に銭湯「サウナの梅湯」があります。初心者にも優しい銭湯です。</p>
+        <p><a href="https://twitter.com/umeyu_rakuen" target="_blank" rel="noopener">詳しくはこちら</a></p>
+      </li>
+      
+    </ul>
+  </div><!-- wrapper -->
+
+  <section class="workation-area">
+    <h2 class="workation-section-title"><span>周辺案内</span>Area</h2>
+    <div class="workation-area-img"></div>
+    <div class="workation-area-text">
+      <p>仕事だけではなくて、<br>
+        京都らしさも味わうのも大切。</p>
+      <p>UNKNOWN KYOTOは<br>
+        あらゆる有名観光地へ<br>
+        電車・バス・自転車などで<br>
+        アクセスしやすい立地です。</p>
+      <p>繁華街からも近く、<br>
+        京都で楽しく食べて飲んだ後は<br>
+        歩いてでも帰れます。</p>
+    </div>
+
+
+
+      <ul class="workation-area-sightseeing">
+        <li>京都駅<br>タクシー/バス7分</li>
+        <li>繁華街エリア<br>徒歩13分</li>
+        <li>鴨川<br>徒歩5分</li>
+        <li>清水寺エリア<br>バス2分</li>
+        <li>伏見稲荷エリア<br>電車7分</li>
+      </ul>
+
+
+  </section>
+
+  <section class="workation-price">
+    <h2 class="workation-section-title"><span>ご利用料金</span>Price</h2>
+    <div class="workation-price-img"></div>
+
+    <div class="workation-price-contents">
+      <ul class="workation-price-basic">
+        <li>
+          <h3 class="workation-price-title">素泊まり</h3>
+          <p class="workation-price-desc">1人ではなく1部屋の価格です。3泊以上からは連泊割引があります。素泊まりでもコワーキングが使えます。</p>
+          <ul class="workation-price-list">
+            <li>ドミトリー<span class="border"></span>3,850円</li>
+            <li>シングル<span class="border"></span>8,250円</li>
+            <li>ツイン<span class="border"></span>9,900円</li>
+            <li class="note">消費税込み、上記意外のお部屋もございます</li>
+          </ul>
+          <p>予約はこちら</p>
+        </li>
+        <li>
+          <h3 class="workation-price-title">ウィークリープラン</h3>
+          <p class="workation-price-desc">6泊以上になるとさらにお安く滞在できます。予約サイトで6泊以上を選択すると自動的に適用されます。</p>
+          <ul class="workation-price-list">
+            <li>ドミトリー<span class="border"></span>17,820円(2,970円/泊)</li>
+            <li>シングル<span class="border"></span>38,214円(6,369円/泊)</li>
+            <li>ツイン<span class="border"></span>45,738円(7,623円/泊)</li>
+            <li class="note">消費税込み、上記意外のお部屋もございます</li>
+          </ul>
+          <p>予約はこちら</p>
+        </li>
+      </ul>
+      <div class="workation-plan">
+        <ul>
+          <li>
+            <div class="workation-plan-img"><img src="<?php echo get_template_directory_uri(); ?>/img/workation/plan-gassyuku.jpg" alt="合宿プラン"></div>
+            <div class="workation-plan-text">
+              <span class="workation-plan-note">おすすめ</span>
+              <h3 class="workation-price-title">合宿プラン</h3>
+              <p class="workation-price-desc">ランチをレストランで取る予定ですとこちらがお得です。</p>
+            </div>
+          </li>
+          <li>
+            <div class="workation-plan-img"><img src="<?php echo get_template_directory_uri(); ?>/img/workation/plan-bungo.jpg" alt="文豪プラン"></div>
+            <div class="workation-plan-text">
+              <span class="workation-plan-note">おすすめ</span>
+              <h3 class="workation-price-title">文豪プラン</h3>
+              <p class="workation-price-desc">ランチをレストランで取る予定ですとこちらがお得です。</p>
+            </div>
+          </li>
+          <li>
+            <div class="workation-plan-img"><img src="<?php echo get_template_directory_uri(); ?>/img/workation/plan-rakuen.jpg" alt="楽園プラン"></div>
+            <div class="workation-plan-text">
+              <h3 class="workation-price-title">楽園プラン</h3>
+              <p class="workation-price-desc">ランチをレストランで取る予定ですとこちらがお得です。</p>
+            </div>
+          </li>
+          <li>
+            <div class="workation-plan-img"><img src="<?php echo get_template_directory_uri(); ?>/img/workation/plan-monthly.jpg" alt="マンスリープラン"></div>
+            <div class="workation-plan-text">
+              <h3 class="workation-price-title">マンスリープラン</h3>
+              <p class="workation-price-desc">1ヶ月単位</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+  
+
+
+    
+
+</div><!-- container -->
+
+<?php get_footer(); ?>
