288ad119fe96915c4279583d4a391bc5f9ebec47
yulily100 <yulily100@users.noreply.github.com>
2022-06-23 11:47:57 +0900

    aboutページ

---
+++ page-about.php
@@ -0,0 +1,93 @@
+<?php get_header(); ?>
+<div class="container">
+	<div class="about-header">
+		<div class="about-header-inner">
+			<h1 class="about-header-title"><a href="/about">About</a></h1>
+		</div>
+	</div>
+
+	<div class="about-contents">
+		<h2 class="about-contents-title">築100年を超える<br class="for-sp">元遊郭建築をリノベーション<br>
+		仕事も食事もできて、<br class="for-sp">暮らせるホステルに</h2>
+		<p>UNKNOWN KYOTOは、河原町五条にあるホステルです。正確な築年数は不詳ですが、明治44年には登記されており少なくとも築110年を超える元遊郭建築を活用しています。</p>
+		<p>改装前の建物には、古いながらも美しく貼られたタイルや手彫りの木の手すり、長い廊下に沿って小部屋が続く元妓楼らしい間取りなど、貴重な造詣がたくさん残されていました。私たちはこれらを文化的な遺産として捉え、できるだけ元の姿を残すことを重視しながら現代人にも過ごしやすいよう改装しました。</p>
+		<p>全部で10室ほどとそう大きくはない宿ですが、同じ建物内にコワーキング、レストラン、シェアキッチン、ランドリーを併設しています。同じ場所で仕事も食事もできて、住んでいるかのように過ごせるホステルです。
+		</p>
+		<p>一般的なホテルというと、観光客と地元の方が交流する機会は少なかったように思います。UNKNOWN KYOTOでは、京都に住む方も利用できるコワーキングスペースを設けることで、それぞれが交わるきっかけを作っていけたらと思っています。</p>
+	</div>
+	<ul class="about-img">
+		<li>
+			<figure>
+				<img src="<?php echo get_template_directory_uri(); ?>/img/about/about-01.jpg" alt="">
+				<figcaption>玄関 改装前</figcaption>
+			</figure>
+		</li>
+		<li>
+			<figure>
+				<img src="<?php echo get_template_directory_uri(); ?>/img/about/about-02.jpg" alt="">
+				<figcaption>コワーキング 改装前</figcaption>
+			</figure>
+		</li>
+	</ul>
+	<div class="about-contents">
+		<h2 class="about-contents-title">高瀬川五条エリアと泰山タイル</h2>
+		<p>五条通り南の高瀬川沿いのエリアは、古くは遊郭やお茶屋が存在しましたが現在では静かな住宅地です。入り組んだ細い路地には明治の妓楼建築や昭和のカフェー建築といった古く個性的な建物が比較的多く残っています。</p>
+		<p>これまであまり手のつけられていない地域でしたが、今後は任天堂元本社ビルの改装を始めとしたエリア活性化プロジェクトの存在や、2023年の市立芸大の移転など、目まぐるしく変化していくことが予想されています。</p>
+		<h3 class="about-contents-title-sub">泰山タイル</h3>
+		<p>大正6年に京都市南区東九条に誕生した「泰山製陶所」。そこで生み出された泰山タイルは、手作りの美術工芸品として国内の歴史的建造物にも多く用いられています。
+			製陶所のあった京都では、格式高い名建築だけでなく市民が日常的に利用するお店などにも取り入れられています。UNKNOWN KYOTO南館のファサードやレストラン、コワーキングスペースにある暖炉には、この泰山タイルが用いられています。特に暖炉には木工の引き出しが組み込まれており、タイルと家具が組み合わされているものは大変めずらしいものだそうです。</p>
+	</div>
+	<ul class="about-img">
+		<li>
+			<figure>
+				<img src="<?php echo get_template_directory_uri(); ?>/img/about/about-03.jpg" alt="">
+				<figcaption>高瀬川五条エリアの町並み</figcaption>
+			</figure>
+		</li>
+		<li>
+			<figure>
+				<img src="<?php echo get_template_directory_uri(); ?>/img/about/about-04.jpg" alt="">
+				<figcaption>泰山タイルが使われた暖炉</figcaption>
+			</figure>
+		</li>
+	</ul>
+	<div class="about-contents">
+		<h2 class="about-contents-title">UNKNOWN KYOTOが<br class="for-sp">大切にしていること</h2>
+		<p>運営しながらひしひしと感じることですが、古い建物を残すのは大変です。大きな改装や耐震工事の費用、スペック的な難しさを考えると、おそらく更地にして新築を建ててしまうほうが簡単でしょう。</p>
+		<p>ですが我々は、古いものと新しいものが混ざり合っていく京都が好きです。歴史ある建物が減ることで、画一的な風景になってしまうことを寂しく思います。UNKNOWN KYOTOを運営することで、微力ながらも多様性のある町並みを残していきたいと思っています。</p>
+		<p>宿としては遮音性や断熱性が少なくご不便をおかけする部分もあるかもしれません。ですが、UNKNOWN KYOTOを利用してくださる方のおかげで、京都に古く貴重な建築が残せている、と思っていただければ嬉しいです。</p>
+	</div>
+
+	<div class="about-contents">
+		<h2 class="about-contents-title">運営体制</h2>
+		<p>UNKNOWN KYOTOは、八清、エンジョイワークス、株式会社ONDの三社で協力しながら企画・運営しています。</p>
+	</div>
+	<ul class="about-company">
+		<li>
+			<p class="about-company-logo"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-ew.png" alt="ENJOYWORKS"></p>
+			<div class="about-company-text">
+				<h3 class="about-company-title">ENJOY WORKS</h3>
+				<p class="about-company-desc">鎌倉を拠点にしながらも、全国各地でまちづくりのプロジェクトをおこなう会社です。</p>
+				<p class="about-company-link"><a href="https://enjoyworks.jp/" target="_blank" rel="noopener">https://enjoyworks.jp/</a></p>
+			</div>
+		</li>
+		<li>
+			<p class="about-company-logo"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-hachise.png" alt="八清"></p>
+			<div class="about-company-text">
+				<h3 class="about-company-title">八清</h3>
+				<p class="about-company-desc">町家リノベーションのといえば真っ先に名前のあがる京都の不動産屋さんです。</p>
+				<p class="about-company-link"><a href="https://www.hachise.jp/" target="_blank" rel="noopener">https://www.hachise.jp/</a></p>
+			</div>
+		</li>
+		<li>
+			<p class="about-company-logo"><img src="<?php echo get_template_directory_uri(); ?>/img/footer-ond.png" alt="株式会社OND"></p>
+			<div class="about-company-text">
+				<h3 class="about-company-title">OND</h3>
+				<p class="about-company-desc">インターネットを使った企画が得意な会社で、個性的な不動産を紹介するサイト「物件ファン」を運営しています。UNKNOWN KYOTOのコワーキング内にオフィスを構え、現場での施設運営も行っています。</p>
+				<p class="about-company-link"><a href="https://ond-inc.com/" target="_blank" rel="noopener">https://ond-inc.com/</a></p>
+			</div>
+		</li>
+	</ul>
+
+</div>
+<?php get_footer(); ?>
